dbd63426f18609ef9dec7485468ea78444cc3bed
Grigory Kirillov <txgk@bk.ru>
2022-12-24 18:46:10 +0300

    improve i2c sniffers a little bit

--- due-i2c-sniffer/due-i2c-sniffer.ino
+++ due-i2c-sniffer/due-i2c-sniffer.ino
@@ -2,37 +2,47 @@
 #define SCL_PIN                          7
 #define SDA                              digitalReadDirect(SDA_PIN)
 #define SCL                              digitalReadDirect(SCL_PIN)
-#define SDA_AND_SCL                      ((digitalReadDirect(SCL_PIN) << 1) | digitalReadDirect(SDA_PIN))
+#define SCL_OR_SDA                       ((digitalReadDirect(SCL_PIN) << 1) | digitalReadDirect(SDA_PIN))
+#define SDA_IS_LOW_AND_SCL_IS_HIGH       (SCL_OR_SDA == 0b10)
+#define SDA_AND_SCL_ARE_HIGH             (SCL_OR_SDA == 0b11)
 #define PACKETS_TO_STORE_BEFORE_PRINTING 10
-#define CHECK_FACTOR                     1
-#define I2C_START_CONDITION_CHECK_FACTOR 1
-#define I2C_START_CONDITION_PASS_FACTOR  1
-#define I2C_SDA_CHECK_FACTOR             1
-#define I2C_SDA_PASS_FACTOR              1
-#define WAIT_SDA_RISE                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SDA == 0); }
-#define WAIT_SDA_DROP                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SDA != 0); }
-#define WAIT_SCL_RISE                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SCL == 0); }
-#define WAIT_SCL_DROP                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SCL != 0); }
-#define WAIT_FOR_BOTH_SDA_AND_SCL_HIGH   for (j = 0; j < CHECK_FACTOR; ++j) { while (SDA_AND_SCL != 0b11); }
-#define WAIT_FOR_SDA_OR_SCL_DROP         for (j = 0; j < CHECK_FACTOR; ++j) { while (SDA_AND_SCL == 0b11); }
-#define WAIT_FOR_SDA_RISE_OR_SCL_DROP    for (j = 0; j < CHECK_FACTOR; ++j) { while (SDA_AND_SCL == 0b10); }
-#define SDA_IS_LOW_AND_SCL_IS_HIGH       (SDA_AND_SCL == 0b10)
-#define SDA_AND_SCL_ARE_HIGH             (SDA_AND_SCL == 0b11)
 #define BYTE_SEPARATOR                   256
 #define MESSAGE_SEPARATOR                512
 #define PACKET_SEPARATOR                 1024
 
+#ifdef CONNECTION_IS_FLAKY
+#define CHECK_FACTOR                     3
+#define I2C_START_CONDITION_CHECK_FACTOR 3
+#define I2C_START_CONDITION_PASS_FACTOR  2
+#define I2C_SDA_CHECK_FACTOR             3
+#define I2C_SDA_PASS_FACTOR              2
+#define WAIT_SDA_RISE                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SDA == 0); }
+#define WAIT_SDA_DROP                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SDA != 0); }
+#define WAIT_SCL_RISE                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SCL == 0); }
+#define WAIT_SCL_DROP                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SCL != 0); }
+#define WAIT_FOR_SDA_AND_SCL_RISE        for (j = 0; j < CHECK_FACTOR; ++j) { while (SCL_OR_SDA != 0b11); }
+#define WAIT_FOR_SDA_OR_SCL_DROP         for (j = 0; j < CHECK_FACTOR; ++j) { while (SCL_OR_SDA == 0b11); }
+#define WAIT_FOR_SDA_RISE_OR_SCL_DROP    for (j = 0; j < CHECK_FACTOR; ++j) { while (SCL_OR_SDA == 0b10); }
+volatile int j, passes;
+#else
+#define WAIT_SDA_RISE                    while (SDA == 0)
+#define WAIT_SDA_DROP                    while (SDA != 0)
+#define WAIT_SCL_RISE                    while (SCL == 0)
+#define WAIT_SCL_DROP                    while (SCL != 0)
+#define WAIT_FOR_SDA_AND_SCL_RISE        while (SCL_OR_SDA != 0b11)
+#define WAIT_FOR_SDA_OR_SCL_DROP         while (SCL_OR_SDA == 0b11)
+#define WAIT_FOR_SDA_RISE_OR_SCL_DROP    while (SCL_OR_SDA == 0b10)
+#endif
+
 volatile uint16_t d[10000];
 volatile uint16_t *p = d;
 volatile uint16_t *ptr;
-volatile int j;
 volatile int packets = 0;
-volatile int passes;
 
 inline uint8_t
 digitalReadDirect(int pin)
 {
-	return !!(g_APinDescription[pin].pPort -> PIO_PDSR & g_APinDescription[pin].ulPin);
+	return !!(g_APinDescription[pin].pPort->PIO_PDSR & g_APinDescription[pin].ulPin);
 }
 
 void
@@ -64,25 +74,33 @@ print_data(void)
 uint8_t
 i2c_start_condition(void)
 {
+#ifdef CONNECTION_IS_FLAKY
 	passes = 0;
 	for (j = 0; j < I2C_START_CONDITION_CHECK_FACTOR; ++j) {
 		if (SDA_IS_LOW_AND_SCL_IS_HIGH) {
 			passes += 1;
 		}
 	}
-	return passes >= I2C_START_CONDITION_PASS_FACTOR ? 1 : 0;
+	return !!(passes >= I2C_START_CONDITION_PASS_FACTOR);
+#else
+	return !!SDA_IS_LOW_AND_SCL_IS_HIGH;
+#endif
 }
 
 uint8_t
-get_sda(void)
+read_sda(void)
 {
+#ifdef CONNECTION_IS_FLAKY
 	passes = 0;
 	for (j = 0; j < I2C_SDA_CHECK_FACTOR; ++j) {
 		if (SDA) {
 			passes += 1;
 		}
 	}
-	return passes >= I2C_SDA_PASS_FACTOR ? 1 : 0;
+	return !!(passes >= I2C_SDA_PASS_FACTOR);
+#else
+	return SDA;
+#endif
 }
 
 void
@@ -97,23 +115,23 @@ void
 loop(void)
 {
 i2c_sync:
-	WAIT_FOR_BOTH_SDA_AND_SCL_HIGH;
+	WAIT_FOR_SDA_AND_SCL_RISE;
 i2c_idle:
 	WAIT_FOR_SDA_OR_SCL_DROP;
 	if (!i2c_start_condition()) goto i2c_sync;
 i2c_start:
 	*p++ = MESSAGE_SEPARATOR;
 	WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p  = get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); p += 1;   WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p  = read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); p += 1;   WAIT_SCL_DROP;
 	WAIT_SCL_RISE;
-	if (get_sda()) {
+	if (read_sda()) {
 		WAIT_SCL_DROP;
 		*p++ = PACKET_SEPARATOR;
 		packets += 1;
@@ -125,17 +143,17 @@ i2c_start:
 		WAIT_SCL_DROP;
 		*p++ = BYTE_SEPARATOR;
 	}
-	WAIT_SCL_RISE; *p  = get_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p  = read_sda(); *p <<= 1; WAIT_SCL_DROP;
 i2c_next:
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); p += 1;   WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); p += 1;   WAIT_SCL_DROP;
 	WAIT_SCL_RISE;
-	if (get_sda()) {
+	if (read_sda()) {
 		*p++ = PACKET_SEPARATOR;
 	} else {
 		*p++ = BYTE_SEPARATOR;
--- esp32-i2c-sniffer/src/main.cpp
+++ esp32-i2c-sniffer/src/main.cpp
@@ -4,42 +4,52 @@
 #define SCL_PIN                          33
 #define SDA                              ((REG_READ(GPIO_IN1_REG)) & 0b01)
 #define SCL                              ((REG_READ(GPIO_IN1_REG)) & 0b10)
-#define SDA_AND_SCL                      ((REG_READ(GPIO_IN1_REG)) & 0b11)
+#define SCL_OR_SDA                       ((REG_READ(GPIO_IN1_REG)) & 0b11)
+#define SDA_IS_LOW_AND_SCL_IS_HIGH       (SCL_OR_SDA == 0b10)
+#define SDA_AND_SCL_ARE_HIGH             (SCL_OR_SDA == 0b11)
 #define PACKETS_TO_STORE_BEFORE_PRINTING 10
-#define CHECK_FACTOR                     30
-#define I2C_START_CONDITION_CHECK_FACTOR 10
-#define I2C_START_CONDITION_PASS_FACTOR  5
-#define I2C_SDA_CHECK_FACTOR             10
-#define I2C_SDA_PASS_FACTOR              5
-#define WAIT_SDA_RISE                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SDA == 0); }
-#define WAIT_SDA_DROP                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SDA != 0); }
-#define WAIT_SCL_RISE                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SCL == 0); }
-#define WAIT_SCL_DROP                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SCL != 0); }
-#define WAIT_FOR_BOTH_SDA_AND_SCL_HIGH   for (j = 0; j < CHECK_FACTOR; ++j) { while (SDA_AND_SCL != 0b11); }
-#define WAIT_FOR_SDA_OR_SCL_DROP         for (j = 0; j < CHECK_FACTOR; ++j) { while (SDA_AND_SCL == 0b11); }
-#define WAIT_FOR_SDA_RISE_OR_SCL_DROP    for (j = 0; j < CHECK_FACTOR; ++j) { while (SDA_AND_SCL == 0b10); }
-#define SDA_IS_LOW_AND_SCL_IS_HIGH       (SDA_AND_SCL == 0b10)
-#define SDA_AND_SCL_ARE_HIGH             (SDA_AND_SCL == 0b11)
 #define BYTE_SEPARATOR                   256
 #define MESSAGE_SEPARATOR                512
 #define PACKET_SEPARATOR                 1024
 
+#ifdef CONNECTION_IS_FLAKY
+#define CHECK_FACTOR                     3
+#define I2C_START_CONDITION_CHECK_FACTOR 3
+#define I2C_START_CONDITION_PASS_FACTOR  2
+#define I2C_SDA_CHECK_FACTOR             3
+#define I2C_SDA_PASS_FACTOR              2
+#define WAIT_SDA_RISE                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SDA == 0); }
+#define WAIT_SDA_DROP                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SDA != 0); }
+#define WAIT_SCL_RISE                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SCL == 0); }
+#define WAIT_SCL_DROP                    for (j = 0; j < CHECK_FACTOR; ++j) { while (SCL != 0); }
+#define WAIT_FOR_SDA_AND_SCL_RISE        for (j = 0; j < CHECK_FACTOR; ++j) { while (SCL_OR_SDA != 0b11); }
+#define WAIT_FOR_SDA_OR_SCL_DROP         for (j = 0; j < CHECK_FACTOR; ++j) { while (SCL_OR_SDA == 0b11); }
+#define WAIT_FOR_SDA_RISE_OR_SCL_DROP    for (j = 0; j < CHECK_FACTOR; ++j) { while (SCL_OR_SDA == 0b10); }
+volatile int j, passes;
+#else
+#define WAIT_SDA_RISE                    while (SDA == 0)
+#define WAIT_SDA_DROP                    while (SDA != 0)
+#define WAIT_SCL_RISE                    while (SCL == 0)
+#define WAIT_SCL_DROP                    while (SCL != 0)
+#define WAIT_FOR_SDA_AND_SCL_RISE        while (SCL_OR_SDA != 0b11)
+#define WAIT_FOR_SDA_OR_SCL_DROP         while (SCL_OR_SDA == 0b11)
+#define WAIT_FOR_SDA_RISE_OR_SCL_DROP    while (SCL_OR_SDA == 0b10)
+#endif
+
 volatile uint16_t d[10000];
 volatile uint16_t *p = d;
 volatile uint16_t *ptr;
-volatile int j;
 volatile int packets = 0;
-volatile int passes;
 
 gpio_config_t cfg = {
 	(1ULL << SDA_PIN) | (1ULL << SCL_PIN),
 	GPIO_MODE_INPUT,
-	GPIO_PULLUP_ENABLE,
+	GPIO_PULLUP_DISABLE,
 	GPIO_PULLDOWN_DISABLE,
 	GPIO_INTR_DISABLE,
 };
 
-static void
+void
 print_data(void)
 {
 	for (ptr = d; ptr < p; ptr += 1) {
@@ -65,34 +75,42 @@ print_data(void)
 	packets = 0;
 }
 
-static uint8_t
+uint8_t
 i2c_start_condition(void)
 {
+#ifdef CONNECTION_IS_FLAKY
 	passes = 0;
 	for (j = 0; j < I2C_START_CONDITION_CHECK_FACTOR; ++j) {
 		if (SDA_IS_LOW_AND_SCL_IS_HIGH) {
 			passes += 1;
 		}
 	}
-	return passes >= I2C_START_CONDITION_PASS_FACTOR ? 1 : 0;
+	return !!(passes >= I2C_START_CONDITION_PASS_FACTOR);
+#else
+	return !!SDA_IS_LOW_AND_SCL_IS_HIGH;
+#endif
 }
 
-static uint8_t
-get_sda(void)
+uint8_t
+read_sda(void)
 {
+#ifdef CONNECTION_IS_FLAKY
 	passes = 0;
 	for (j = 0; j < I2C_SDA_CHECK_FACTOR; ++j) {
 		if (SDA) {
 			passes += 1;
 		}
 	}
-	return passes >= I2C_SDA_PASS_FACTOR ? 1 : 0;
+	return !!(passes >= I2C_SDA_PASS_FACTOR);
+#else
+	return SDA;
+#endif
 }
 
 void
 setup(void)
 {
-	Serial.begin(460800);
+	Serial.begin(115200);
 	gpio_config(&cfg);
 }
 
@@ -100,23 +118,23 @@ void
 loop(void)
 {
 i2c_sync:
-	WAIT_FOR_BOTH_SDA_AND_SCL_HIGH;
+	WAIT_FOR_SDA_AND_SCL_RISE;
 i2c_idle:
 	WAIT_FOR_SDA_OR_SCL_DROP;
 	if (!i2c_start_condition()) goto i2c_sync;
 i2c_start:
 	*p++ = MESSAGE_SEPARATOR;
 	WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p  = get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); p += 1;   WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p  = read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); p += 1;   WAIT_SCL_DROP;
 	WAIT_SCL_RISE;
-	if (get_sda()) {
+	if (read_sda()) {
 		WAIT_SCL_DROP;
 		*p++ = PACKET_SEPARATOR;
 		packets += 1;
@@ -128,17 +146,17 @@ i2c_start:
 		WAIT_SCL_DROP;
 		*p++ = BYTE_SEPARATOR;
 	}
-	WAIT_SCL_RISE; *p  = get_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p  = read_sda(); *p <<= 1; WAIT_SCL_DROP;
 i2c_next:
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); *p <<= 1; WAIT_SCL_DROP;
-	WAIT_SCL_RISE; *p |= get_sda(); p += 1;   WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); *p <<= 1; WAIT_SCL_DROP;
+	WAIT_SCL_RISE; *p |= read_sda(); p += 1;   WAIT_SCL_DROP;
 	WAIT_SCL_RISE;
-	if (get_sda()) {
+	if (read_sda()) {
 		*p++ = PACKET_SEPARATOR;
 	} else {
 		*p++ = BYTE_SEPARATOR;
