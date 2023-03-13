653f194877b45fae6c87c1b45de1a933ea93e3dd
Grigory Kirillov <txgk@bk.ru>
2023-03-14 00:56:07 +0300

    improve arduino i2c meter a bit

--- arduino-i2c-meter/arduino-i2c-meter.ino
+++ arduino-i2c-meter/arduino-i2c-meter.ino
@@ -7,11 +7,16 @@ size_t i;
 int voltage, voltage_max;
 unsigned long start_time, end_time;
 double frequency;
+double frequency_avg = 0;
+size_t frequency_count = 0;
 
 void setup() {
 	Serial.begin(9600);
 	pinMode(SCL_FREQ_PIN, INPUT);
 }
+
+int periods_per_measure = 4;
+int periods_cycles = 1000;
 
 void loop() {
 	// Measure voltage
@@ -23,19 +28,28 @@ void loop() {
 		}
 	}
 	Serial.print("Voltage is: ");
-	Serial.println(voltage_max);
+	Serial.println(voltage_max * 5.0 / 1024);
 	// Measure frequency
-	WAIT_FOR_DIGITAL_HIGH;
-	WAIT_FOR_DIGITAL_LOW;
-	start_time = micros();
-	for (i = 0; i < 1000; ++i) {
+	frequency_count = 0;
+	frequency_avg = 0;
+	for (size_t i = 0; i < periods_cycles; ++i) {
 		WAIT_FOR_DIGITAL_HIGH;
 		WAIT_FOR_DIGITAL_LOW;
+		start_time = micros();
+		for (i = 0; i < periods_per_measure; ++i) {
+			WAIT_FOR_DIGITAL_HIGH;
+			WAIT_FOR_DIGITAL_LOW;
+		}
+		end_time = micros();
+		frequency = periods_per_measure * 1000000.0 / (end_time - start_time);
+		frequency_avg = frequency_avg * frequency_count + frequency;
+		frequency_count += 1;
+		frequency_avg /= frequency_count;
+		Serial.print("Average frequency is: ");
+		Serial.print(frequency_avg);
+		Serial.print(" Current frequency is: ");
+		Serial.println(frequency);
 	}
-	end_time = micros();
-	frequency = 1000000000.0 / (end_time - start_time);
-	Serial.print("Frequency is: ");
-	Serial.println(frequency);
 	// Rest a little
 	delay(500);
 }
