bba1584fc1c2753b4e7187494744346e8f92c29a
Grigory Kirillov <txgk@bk.ru>
2023-06-07 00:59:54 +0300

    change plotting library to chart.js

--- esp32-project-nosedive/make-offline-monitor-page.sh
+++ esp32-project-nosedive/make-offline-monitor-page.sh
@@ -3,13 +3,13 @@ cd "$(dirname "$0")" || exit 1
 monitor_file='monitor.html'
 monitor_offline_file='monitor-offline.html'
 monitor_offline_file_compressed="${monitor_offline_file}.gz"
-plotly_file='plotly-latest.min.js'
-plotly_file_source='https://cdn.plot.ly/plotly-latest.min.js'
-[ -e "$plotly_file" ] || wget -O "$plotly_file" "$plotly_file_source"
-[ -e "$plotly_file" ] || exit 1
+chartjs_file='chart.umd.js'
+chartjs_file_source='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.3.0/chart.umd.js'
+[ -e "$chartjs_file" ] || wget -O "$chartjs_file" "$chartjs_file_source"
+[ -e "$chartjs_file" ] || exit 1
 head -n -2 "$monitor_file" > "$monitor_offline_file"
-sed -i '/plotly-latest.min.js/d' "$monitor_offline_file"
+sed -i '/jsdelivr.net/d' "$monitor_offline_file"
 echo '<script>' >> "$monitor_offline_file"
-cat "$plotly_file" >> "$monitor_offline_file"
+cat "$chartjs_file" >> "$monitor_offline_file"
 echo '</script></body></html>' >> "$monitor_offline_file"
 gzip --best -c "$monitor_offline_file" > main/"$monitor_offline_file_compressed"
