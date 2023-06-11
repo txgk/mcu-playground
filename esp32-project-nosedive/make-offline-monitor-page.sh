0638ad60e7b7d1bed6b86e8c2b69c649ed4f87df
Grigory Kirillov <txgk@bk.ru>
2023-06-11 11:00:16 +0300

    bundle chart.js library

--- esp32-project-nosedive/make-offline-monitor-page.sh
+++ esp32-project-nosedive/make-offline-monitor-page.sh
@@ -4,9 +4,6 @@ monitor_file='monitor.html'
 monitor_offline_file='monitor-offline.html'
 monitor_offline_file_compressed="${monitor_offline_file}.gz"
 chartjs_file='chart.umd.js'
-chartjs_file_source='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.3.0/chart.umd.js'
-[ -e "$chartjs_file" ] || wget -O "$chartjs_file" "$chartjs_file_source"
-[ -e "$chartjs_file" ] || exit 1
 head -n -2 "$monitor_file" > "$monitor_offline_file"
 sed -i '/jsdelivr.net/d' "$monitor_offline_file"
 echo '<script>' >> "$monitor_offline_file"
