364ee7e9407e60969de95b798b674c990d138dc6
Grigory Kirillov <txgk@bk.ru>
2023-06-17 02:38:09 +0300

    add script for preparing release binary

---
+++ esp32-project-nosedive/prepare-release-binary.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+file_name="release-$(date +'%Y-%m-%d').bin"
+cd "$(dirname "$0")"/build || exit 1
+flash_args="$(cat flash_args)"
+esptool.py --chip esp32 merge_bin -o ../"$file_name" $flash_args
