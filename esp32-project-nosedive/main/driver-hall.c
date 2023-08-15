699ca43df1b70d23d503bacc9ff5fb121b1c3012
Grigory Kirillov <txgk@bk.ru>
2023-08-15 06:01:23 +0300

    sync isr and task of hall driver

--- esp32-project-nosedive/main/driver-hall.c
+++ esp32-project-nosedive/main/driver-hall.c
@@ -4,48 +4,35 @@
 #define HALL_SAMPLES_COUNT         100
 #define HALL_MEASUREMENT_PERIOD_US 5000000
 
-static volatile _Atomic int64_t hall_samples[HALL_SAMPLES_COUNT];
-static volatile _Atomic int64_t last_sample_time = 0;
-static volatile _Atomic size_t hall_iter = 0;
-static volatile _Atomic bool hall_in_use = false;
-static volatile double hall_freq         = 0;
+static volatile SemaphoreHandle_t hall_binary_semaphore = NULL;
+static volatile int64_t hall_samples[HALL_SAMPLES_COUNT];
+static volatile int64_t last_sample_time = 0;
+static volatile size_t hall_iter = 0;
 
 void IRAM_ATTR
 hall_take_sample(void *dummy)
 {
-	hall_in_use = true;
-	last_sample_time = esp_timer_get_time();
-	hall_samples[hall_iter] = last_sample_time;
-	hall_iter = (hall_iter + 1) % HALL_SAMPLES_COUNT;
-	size_t fresh_samples_count = 0;
-	int64_t oldest_sample_time = last_sample_time;
-	for (size_t i = 0; i < HALL_SAMPLES_COUNT; ++i) {
-		if (last_sample_time - hall_samples[i] < HALL_MEASUREMENT_PERIOD_US) {
-			fresh_samples_count += 1;
-			if (hall_samples[i] < oldest_sample_time) {
-				oldest_sample_time = hall_samples[i];
-			}
-		}
+	if (hall_binary_semaphore != NULL) {
+		xSemaphoreGiveFromISR(hall_binary_semaphore, NULL);
 	}
-	// Частота = множитель мкс * кол-во измерений за период / период съёма измерений в мкс
-	hall_freq = 1000000.0 * fresh_samples_count / (last_sample_time - oldest_sample_time);
-	hall_in_use = false;
 }
 
 void IRAM_ATTR
 hall_task(void *arg)
 {
 	struct task_descriptor *task = arg;
-	while (true) {
-		if (xSemaphoreTake(task->mutex, portMAX_DELAY) == pdTRUE) {
-			while (hall_in_use == true);
-			if ((esp_timer_get_time() - last_sample_time > HALL_MEASUREMENT_PERIOD_US) && (hall_in_use == false)) {
-				last_sample_time = esp_timer_get_time();
-				hall_freq = 0;
+	hall_binary_semaphore = xSemaphoreCreateBinary();
+	if (hall_binary_semaphore != NULL) {
+		while (true) {
+			if (xSemaphoreTake(hall_binary_semaphore, portMAX_DELAY) == pdTRUE) {
+				if (xSemaphoreTake(task->mutex, portMAX_DELAY) == pdTRUE) {
+					last_sample_time = esp_timer_get_time();
+					hall_samples[hall_iter] = last_sample_time;
+					hall_iter = (hall_iter + 1) % HALL_SAMPLES_COUNT;
+					xSemaphoreGive(task->mutex);
+				}
 			}
-			xSemaphoreGive(task->mutex);
 		}
-		TASK_DELAY_MS(task->performer_period_ms);
 	}
 	vTaskDelete(NULL);
 }
@@ -53,16 +40,33 @@ hall_task(void *arg)
 int
 hall_info(struct task_descriptor *task, char *dest)
 {
-	int len = 0;
+	const int64_t current_time = esp_timer_get_time();
+	double hall_freq = 0;
 	if (xSemaphoreTake(task->mutex, portMAX_DELAY) == pdTRUE) {
-		while (hall_in_use == true);
-		len = snprintf(dest, MESSAGE_SIZE_LIMIT,
-			"%s@%lld=%.2lf\n",
-			task->prefix,
-			last_sample_time / 1000,
-			hall_freq * 60 // Convert to RPM.
-		);
+		if (current_time - last_sample_time <= HALL_MEASUREMENT_PERIOD_US) {
+			size_t fresh_samples_count = 0;
+			int64_t oldest_sample_time = last_sample_time;
+			for (size_t i = 0; i < HALL_SAMPLES_COUNT; ++i) {
+				if (current_time - hall_samples[i] <= HALL_MEASUREMENT_PERIOD_US) {
+					fresh_samples_count += 1;
+					if (hall_samples[i] < oldest_sample_time) {
+						oldest_sample_time = hall_samples[i];
+					}
+				}
+			}
+			if (last_sample_time > oldest_sample_time) {
+				// Частота = множитель мкс * кол-во измерений за период / период съёма измерений в мкс
+				hall_freq = 1000000.0 * fresh_samples_count / (last_sample_time - oldest_sample_time);
+			}
+		}
 		xSemaphoreGive(task->mutex);
 	}
+	int len = 0;
+	len = snprintf(dest, MESSAGE_SIZE_LIMIT,
+		"%s@%lld=%.2lf\n",
+		task->prefix,
+		current_time / 1000,
+		hall_freq * 60 // Convert to RPM.
+	);
 	return len > 0 && len < MESSAGE_SIZE_LIMIT ? len : 0;
 }
