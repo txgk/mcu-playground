a86a987eb88c2d52eff2f0e0d8a22a11c57abdbb
Grigory Kirillov <txgk@bk.ru>
2023-09-03 18:14:02 +0300

    disable tcp streamer

--- esp32-project-candela/main/main.c
+++ esp32-project-candela/main/main.c
@@ -141,7 +141,6 @@ app_main(void)
 	start_tasks();
 
 	start_serial_streamer();
-	start_tcp_streamer();
 	start_websocket_streamer();
 	start_http_tuner();
 	start_can_listener();
@@ -150,7 +149,6 @@ app_main(void)
 	// char heartbeat_text_buf[100];
 	// int32_t i = 1;
 	while (they_want_us_to_restart == false) {
-		write_tcp_message("Hi!\n", 4);
 		// int64_t ms = esp_timer_get_time() / 1000;
 		// int heartbeat_text_len = snprintf(heartbeat_text_buf, 100, "BEAT@%lld=%ld\n", ms, i);
 		// if (heartbeat_text_len > 0 && heartbeat_text_len < 100) {
